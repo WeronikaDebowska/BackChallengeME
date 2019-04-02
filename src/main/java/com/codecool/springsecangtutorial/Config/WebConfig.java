e57d7d72d685c52b56260001288b97e8a2241ef1
WeronikaDebowska <weronika.debowska@gmail.com>
2019-04-02 14:36:19 +0200

    initial commit

---
+++ src/main/java/com/codecool/springsecangtutorial/Config/WebConfig.java
@@ -0,0 +1,19 @@
+//package com.codecool.springsecangtutorial.Config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors();
+//    }
+//}
---
+++ src/main/java/com/codecool/springsecangtutorial/SpringSecAngTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.springsecangtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecAngTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecAngTutorialApplication.class, args);
+        System.out.println("START");
+    }
+
+}
