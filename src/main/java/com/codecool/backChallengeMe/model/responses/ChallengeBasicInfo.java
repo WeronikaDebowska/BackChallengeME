4e81546fe7915edb14aa37c5f717590d152b78c8
WeronikaDebowska <weronika.debowska@gmail.com>
2019-05-20 17:19:40 +0200

    ChallengeBasicInfo Class

---
+++ src/main/java/com/codecool/backChallengeMe/model/responses/ChallengeBasicInfo.java
@@ -0,0 +1,45 @@
+package com.codecool.backChallengeMe.model.responses;
+
+
+import com.codecool.backChallengeMe.model.Challenge;
+import com.codecool.backChallengeMe.model.Tag;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@Scope(value = "prototype")
+@Getter
+@Setter
+public class ChallengeBasicInfo {
+
+    private Long challengeId;
+    private String challengeName;
+
+    private String status;
+
+    private Timestamp start;
+    private Timestamp finish;
+
+    private List<String> tagList;
+
+    public ChallengeBasicInfo() {
+    }
+
+    public ChallengeBasicInfo(Challenge challenge) {
+        this.challengeId = challenge.getId();
+        this.challengeName = challenge.getName();
+        this.start = challenge.getStart();
+        this.finish = challenge.getFinish();
+        this.tagList = challenge.getChallengeTagList().stream()
+                .map(Tag::getTagName)
+                .collect(Collectors.toList());
+    }
+}
+
+
